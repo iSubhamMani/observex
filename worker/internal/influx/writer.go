69336c668e300b1d1060b95114a89f7b21c718f2
Subham Mani <manisubham09@gmail.com>
2026-03-15 22:44:38 +0530

    add InfluxWriter for writing system metrics to InfluxDB

---
+++ worker/internal/influx/writer.go
@@ -0,0 +1,59 @@
+package influx
+
+import (
+	"context"
+	"time"
+
+	influxdb3 "github.com/InfluxCommunity/influxdb3-go/v2/influxdb3"
+)
+
+type InfluxWriter struct {
+	Client   *influxdb3.Client
+	Database string
+}
+
+func NewInfluxWriter(url, token, database string) (*InfluxWriter, error) {
+
+	client, err := influxdb3.New(influxdb3.ClientConfig{
+		Host:  url,
+		Token: token,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &InfluxWriter{
+		Client:   client,
+		Database: database,
+	}, nil
+}
+
+func (w *InfluxWriter) Close() error {
+	return w.Client.Close()
+}
+
+func (w *InfluxWriter) WriteMetric(
+	ctx context.Context,
+	host string,
+	cpu float64,
+	disk float64,
+	mem float64,
+	net float64,
+	timestamp int64,
+) error {
+
+	point := influxdb3.NewPointWithMeasurement("system_metrics").
+		SetTag("host", host).
+		SetDoubleField("cpu_usage", cpu).
+		SetDoubleField("disk_usage", disk).
+		SetDoubleField("memory_usage", mem).
+		SetDoubleField("net_usage", net).
+		SetTimestamp(time.Unix(timestamp, 0))
+
+	return w.Client.WritePoints(
+		ctx,
+		[]*influxdb3.Point{point},
+		influxdb3.WithDatabase(w.Database),
+	)
+}
