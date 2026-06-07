86cb1a53255cdb07bd0bec0f0778146d7332733c
Subham Mani <manisubham09@gmail.com>
2026-06-08 00:37:23 +0530

    add toast

--- app/components/Logout.tsx
+++ app/components/Logout.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import axios from "axios";
+import { useToast } from "@/hooks/useToast";
+import axios, { isAxiosError } from "axios";
 import { useRouter } from "next/navigation";
 
 const Logout = () => {
   const router = useRouter();
+  const { showToast } = useToast();
 
   return (
     <button
       onClick={async () => {
         try {
           await axios.post("/api/auth/logout");
+          showToast("success", "Logged out successfully!");
           router.replace("/login");
         } catch (error) {
-          console.log("Logout failed", error);
+          if (isAxiosError(error)) {
+            showToast(
+              "error",
+              error.response?.data.error || "Logout failed. Please try again.",
+            );
+          }
         }
       }}
       className="hover:cursor-pointer rounded-md border border-border px-3 py-1.5 text-sm font-medium hover:bg-accent hover:text-accent-foreground"
