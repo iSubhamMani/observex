688e311048827df3d1362deaa3f0b749dea5e297
Subham Mani <manisubham09@gmail.com>
2026-06-11 23:40:55 +0530

    add demo data

---
+++ app/lib/demoData.ts
@@ -0,0 +1,215 @@
+export const demoSeries = [
+  { date: "2026-06-05", views: 420, visitors: 180, bounce_rate: 42 },
+  { date: "2026-06-06", views: 510, visitors: 230, bounce_rate: 39 },
+  { date: "2026-06-07", views: 680, visitors: 290, bounce_rate: 44 },
+  { date: "2026-06-08", views: 730, visitors: 315, bounce_rate: 40 },
+  { date: "2026-06-09", views: 940, visitors: 410, bounce_rate: 37 },
+  { date: "2026-06-10", views: 1200, visitors: 520, bounce_rate: 35 },
+  { date: "2026-06-11", views: 1480, visitors: 610, bounce_rate: 33 },
+];
+
+export const demoTotals = {
+  unique_visitors: 2555,
+  total_pageviews: 5960,
+  bounce_rate: 38,
+  average_duration_seconds: 142,
+
+  unique_visitors_delta: 18.4,
+  total_pageviews_delta: 24.1,
+  bounce_rate_delta: -4.2,
+  average_duration_delta: 12.7,
+};
+
+export const demoTopPages = [
+  {
+    path: "/",
+    views: 2140,
+    visitors: 1120,
+  },
+  {
+    path: "/pricing",
+    views: 1430,
+    visitors: 980,
+  },
+  {
+    path: "/blog/web-analytics-guide",
+    views: 980,
+    visitors: 710,
+  },
+  {
+    path: "/docs",
+    views: 740,
+    visitors: 520,
+  },
+  {
+    path: "/contact",
+    views: 320,
+    visitors: 280,
+  },
+];
+
+export const demoReferrers = [
+  {
+    name: "Google",
+    visitors: 1042,
+    views: 2240,
+  },
+  {
+    name: "Twitter",
+    visitors: 582,
+    views: 1010,
+  },
+  {
+    name: "LinkedIn",
+    visitors: 421,
+    views: 812,
+  },
+  {
+    name: "GitHub",
+    visitors: 268,
+    views: 530,
+  },
+  {
+    name: "Direct",
+    visitors: 242,
+    views: 420,
+  },
+];
+
+export const demoCountries = [
+  {
+    code: "IN",
+    name: "India",
+    visitors: 1260,
+  },
+  {
+    code: "US",
+    name: "United States",
+    visitors: 520,
+  },
+  {
+    code: "GB",
+    name: "United Kingdom",
+    visitors: 290,
+  },
+  {
+    code: "DE",
+    name: "Germany",
+    visitors: 180,
+  },
+  {
+    code: "SG",
+    name: "Singapore",
+    visitors: 140,
+  },
+];
+
+export const demoBrowsers = [
+  {
+    name: "Chrome",
+    visitors: 1780,
+  },
+  {
+    name: "Safari",
+    visitors: 320,
+  },
+  {
+    name: "Firefox",
+    visitors: 210,
+  },
+  {
+    name: "Edge",
+    visitors: 160,
+  },
+  {
+    name: "Opera",
+    visitors: 85,
+  },
+];
+export const demoDevices = [
+  {
+    name: "desktop",
+    visitors: 1450,
+  },
+  {
+    name: "mobile",
+    visitors: 940,
+  },
+  {
+    name: "tablet",
+    visitors: 165,
+  },
+];
+
+export const demoCustomEvents = [
+  {
+    event_name: "signup_started",
+    total_events: 425,
+  },
+  {
+    event_name: "signup_completed",
+    total_events: 312,
+  },
+  {
+    event_name: "checkout_started",
+    total_events: 208,
+  },
+  {
+    event_name: "purchase_completed",
+    total_events: 94,
+  },
+  {
+    event_name: "cta_clicked",
+    total_events: 756,
+  },
+];
+
+export const demoEventKeys = ["plan", "button_text", "source"];
+
+export const demoEventMeta = [
+  {
+    property_value: "Pro",
+    unique_visitors: 162,
+    total_actions: 210,
+  },
+  {
+    property_value: "Starter",
+    unique_visitors: 118,
+    total_actions: 148,
+  },
+  {
+    property_value: "Enterprise",
+    unique_visitors: 32,
+    total_actions: 41,
+  },
+];
+
+export const demoUtmCampaigns = [
+  {
+    name: "launch_week",
+    visitors: 920,
+    views: 2130,
+  },
+  {
+    name: "product_hunt",
+    visitors: 580,
+    views: 1320,
+  },
+  {
+    name: "summer_sale",
+    visitors: 430,
+    views: 970,
+  },
+  {
+    name: "newsletter_june",
+    visitors: 310,
+    views: 720,
+  },
+];
+
+export const demoProject = {
+  id: "123",
+  name: "Demo Project",
+  domain: "demo.observex.com",
+  createdAt: Date.now(),
+};
